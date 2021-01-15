fd2dc7e4a16a9592be4b7ac6f1adf617aac814b3
Matthew R. Becker <beckermr@users.noreply.github.com>
2021-01-15 12:38:18 -0600

    Update recipe/deactivate-clang.sh

--- recipe/deactivate-clang.sh
+++ recipe/deactivate-clang.sh
@@ -119,6 +119,7 @@ fi
 _tc_activation \
   deactivate @CHOST@- "HOST,@CHOST@" \
   "CONDA_TOOLCHAIN_HOST,@CHOST@" \
+  "CONDA_TOOLCHAIN_BUILD,@CBUILD@" \
   ar as checksyms indr install_name_tool libtool lipo nm nmedit otool \
   pagestuff ranlib redo_prebinding seg_addr_table seg_hack segedit size strings strip \
   clang ld \
