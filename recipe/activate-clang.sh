635616cb4e4e8a4e8b18435cc9e1f7572e51e46d
Matthew R. Becker <beckermr@users.noreply.github.com>
2021-05-10 11:05:11 -0500

    Update recipe/activate-clang.sh

--- recipe/activate-clang.sh
+++ recipe/activate-clang.sh
@@ -121,11 +121,11 @@ fi
 
 CONDA_BUILD_SYSROOT_TEMP=${CONDA_BUILD_SYSROOT:-${SDKROOT:-0}}
 if [ "${CONDA_BUILD_SYSROOT_TEMP}" = "0" ]; then
-if [ "${SDKROOT:-0}" = "0" ]; then
-  CONDA_BUILD_SYSROOT_TEMP=$(xcrun --show-sdk-path)
-else
-  CONDA_BUILD_SYSROOT_TEMP=${SDKROOT}
-fi
+  if [ "${SDKROOT:-0}" = "0" ]; then
+    CONDA_BUILD_SYSROOT_TEMP=$(xcrun --show-sdk-path)
+  else
+    CONDA_BUILD_SYSROOT_TEMP=${SDKROOT}
+  fi
 fi
 
 _CMAKE_ARGS="-DCMAKE_AR=${CONDA_PREFIX}/bin/@CHOST@-ar -DCMAKE_CXX_COMPILER_AR=${CONDA_PREFIX}/bin/@CHOST@-ar -DCMAKE_C_COMPILER_AR=${CONDA_PREFIX}/bin/@CHOST@-ar"
