34923e037a9045cd7f53001d72c2f60c804f9c1d
Ralf Gommers <ralf.gommers@gmail.com>
2022-09-12 07:32:45 +0300

    Remove `-fPIE` compile and `-fpie` link flags
    Closes gh-88

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,8 @@
 CHOST=${macos_machine}
 
 FINAL_CPPFLAGS="-D_FORTIFY_SOURCE=2"
-FINAL_CFLAGS="-ftree-vectorize -fPIC -fPIE -fstack-protector-strong -O2 -pipe"
-FINAL_CXXFLAGS="-ftree-vectorize -fPIC -fPIE -fstack-protector-strong -O2 -pipe -stdlib=libc++ -fvisibility-inlines-hidden -fmessage-length=0"
+FINAL_CFLAGS="-ftree-vectorize -fPIC -fstack-protector-strong -O2 -pipe"
+FINAL_CXXFLAGS="-ftree-vectorize -fPIC -fstack-protector-strong -O2 -pipe -stdlib=libc++ -fvisibility-inlines-hidden -fmessage-length=0"
 if [[ "${version}" == "11.1.0" ]]; then
   FINAL_CXXFLAGS="${FINAL_CXXFLAGS} -std=c++14"
 fi
@@ -13,9 +13,9 @@ if [[ "${uname_machine}" == "x86_64" ]]; then
   FINAL_CXXFLAGS="-march=core2 -mtune=haswell -mssse3 $FINAL_CXXFLAGS"
 fi
 # These are the LDFLAGS for when the linker is being driven by a compiler, i.e. with -Wl,
-FINAL_LDFLAGS="-Wl,-pie -Wl,-headerpad_max_install_names -Wl,-dead_strip_dylibs"
+FINAL_LDFLAGS="-Wl,-headerpad_max_install_names -Wl,-dead_strip_dylibs"
 # These are the LDFLAGS for when the linker is being called directly, i.e. without -Wl,
-FINAL_LDFLAGS_LD="-pie -headerpad_max_install_names -dead_strip_dylibs"
+FINAL_LDFLAGS_LD="-headerpad_max_install_names -dead_strip_dylibs"
 FINAL_DEBUG_CFLAGS="-Og -g -Wall -Wextra"
 FINAL_DEBUG_CXXFLAGS="-Og -g -Wall -Wextra"
 
