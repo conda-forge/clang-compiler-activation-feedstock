f1ce56207ab06b57e3316c8556fac77db583a665
H. Vetinari <h.vetinari@gmx.com>
2023-03-21 12:24:27 +1100

    extend ${BUILD_PREFIX}/meson_cross_file.txt with pkgconfig & cmake setup

--- recipe/activate-clang.sh
+++ recipe/activate-clang.sh
@@ -153,6 +153,14 @@ if [ "@CONDA_BUILD_CROSS_COMPILATION@" = "1" ]; then
   echo "cpu = '@UNAME_MACHINE@'" >> ${CONDA_PREFIX}/meson_cross_file.txt
   echo "cpu_family = '@MESON_CPU_FAMILY@'" >> ${CONDA_PREFIX}/meson_cross_file.txt
   echo "endian = 'little'" >> ${CONDA_PREFIX}/meson_cross_file.txt
+  # specify path to correct binaries/metadata the meson will not auto-discover out of caution
+  # binaries from build env
+  echo "[binaries]" >> ${CONDA_PREFIX}/meson_cross_file.txt
+  echo "cmake = '${CONDA_PREFIX}/bin/cmake'" >> ${CONDA_PREFIX}/meson_cross_file.txt
+  echo "pkgconfig = '${CONDA_PREFIX}/bin/pkg-config'" >> ${CONDA_PREFIX}/meson_cross_file.txt
+  # metadata from host env
+  echo "[properties]" >> ${CONDA_PREFIX}/meson_cross_file.txt
+  echo "pkg_config_libdir = '${PREFIX}/lib'" >> ${CONDA_PREFIX}/meson_cross_file.txt
 fi
 
 _tc_activation \
