f2e8fa017f70bee6cba06b9dd25b549acd8f76c8
H. Vetinari <h.vetinari@gmx.com>
2023-12-03 13:41:58 +1100

    don't try to create already-existing symlink

--- recipe/install-clangxx.sh
+++ recipe/install-clangxx.sh
@@ -7,7 +7,9 @@ echo CHOST is ${CHOST}
 
 pushd "${PREFIX}"/bin
   ln -s clang++ ${CHOST}-clang++
-  if [[ "${CHOST}" != "${CBUILD}" ]]; then
+  if [[ "${CHOST}" != "${CBUILD}" ]] && [[ "${target_platform}" != linux-* ]]; then
+    # on linux, the `clangxx` package already has a $TRIPLE-clang++, see
+    # https://github.com/conda-forge/clangdev-feedstock/pull/251
     ln -s clang++ ${CBUILD}-clang++
   fi
 popd
