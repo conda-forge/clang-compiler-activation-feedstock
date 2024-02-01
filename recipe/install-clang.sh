28306e09029259088ba3983ddd80b021e7f9105a
H. Vetinari <h.vetinari@gmx.com>
2024-02-01 13:48:48 +0100

    drop LLVM 15

--- recipe/install-clang.sh
+++ recipe/install-clang.sh
@@ -6,7 +6,7 @@ CHOST=${macos_machine}
 
 pushd "${PREFIX}"/bin
   ln -s clang ${CHOST}-clang
-  if [[ "${CBUILD}" != ${CHOST} ]] && [[ "${target_platform}" != linux-* || ${version} == "15.0.7" ]]; then
+  if [[ "${CBUILD}" != ${CHOST} ]] && [[ "${target_platform}" != linux-* ]]; then
     # on linux, the `clang` package already has a $TRIPLE-clang, see
     # https://github.com/conda-forge/clangdev-feedstock/pull/251
     ln -s clang ${CBUILD}-clang
