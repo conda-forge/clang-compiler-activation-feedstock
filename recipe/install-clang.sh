83017bd515b246059382710d33880c373ec1b931
H. Vetinari <h.vetinari@gmx.com>
2024-07-01 16:36:13 +1100

    add symlinks for $TRIPLE-clang-cpp

--- recipe/install-clang.sh
+++ recipe/install-clang.sh
@@ -6,9 +6,11 @@ CHOST=${macos_machine}
 
 pushd "${PREFIX}"/bin
   ln -s clang ${CHOST}-clang
+  ln -s clang-cpp ${CHOST}-clang-cpp
   if [[ "${CBUILD}" != ${CHOST} ]] && [[ "${target_platform}" != linux-* ]]; then
     # on linux, the `clang` package already has a $TRIPLE-clang, see
     # https://github.com/conda-forge/clangdev-feedstock/pull/251
     ln -s clang ${CBUILD}-clang
+    ln -s clang-cpp ${CBUILD}-clang-cpp
   fi
 popd
