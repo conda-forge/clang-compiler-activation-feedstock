6dd8c3c2855c42777b8bdfb7a10366ca5c594230
H. Vetinari <h.vetinari@gmx.com>
2024-11-19 10:02:41 +1100

    match changes on deactivation side

--- recipe/deactivate-clang++.sh
+++ recipe/deactivate-clang++.sh
@@ -100,7 +100,7 @@ fi
 
 _tc_activation \
   deactivate @CHOST@- \
-  clang++ \
+  "CLANGXX,${CXX:-@CHOST@-clang++}" \
   "CXX,${CXX:-@CHOST@-clang++}" \
   "CXX_FOR_BUILD,${CONDA_PREFIX}/bin/@CXX_FOR_BUILD@" \
   "CXXFLAGS,${CXXFLAGS:-${CXXFLAGS_USED}}" \
