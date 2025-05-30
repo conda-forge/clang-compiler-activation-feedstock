99c225bd11c40dcd242c09a8fadb490007e4690c
H. Vetinari <h.vetinari@gmx.com>
2025-05-30 13:20:33 +1100

    remove version checks in `install-clang{,xx}_impl.sh`
    clang 17 isn't being built anymore, and the relevant change was backported to clang 18, see
    https://github.com/conda-forge/clangdev-feedstock/commit/66757f6ac00ceccb8af07d6dcc4b05c4d5efbcdf

--- recipe/install-clangxx_impl.sh
+++ recipe/install-clangxx_impl.sh
@@ -7,9 +7,7 @@ echo CHOST is ${CHOST}
 
 pushd "${PREFIX}"/bin
   ln -s clang++ ${CHOST}-clang++
-  if [[ "${CBUILD}" != ${CHOST} ]] && [[ "${target_platform}" != linux-* || ( ${version} != "17.0.6" && ${version} != "18.1.8") ]]; then
-    # before v19.1.1, `clangxx` on linux already had this symlink, see
-    # https://github.com/conda-forge/clangdev-feedstock/pull/322
+  if [[ "${CBUILD}" != ${CHOST} ]]; then
     ln -s clang++ ${CBUILD}-clang++
   fi
 popd
