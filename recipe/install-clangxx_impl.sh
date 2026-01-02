a6dc1d9a112ac94362cf4d21b07bdc7e9f491420
Isuru Fernando <isuruf@gmail.com>
2026-01-02 14:38:46 -0800

    fix clangxx

--- recipe/install-clangxx_impl.sh
+++ recipe/install-clangxx_impl.sh
@@ -2,10 +2,11 @@
 
 set -e -x
 
+MAJOR_VERSION=$(echo ${PKG_VERSION} | cut -f1 -d".")
 CHOST=${macos_machine}
 echo CHOST is ${CHOST}
 
 mkdir -p "${PREFIX}/bin"
 pushd "${PREFIX}/bin"
-  ln -s clang++ ${CHOST}-clang++
+  ln -s clang-${MAJOR_VERSION} ${CHOST}-clang++
 popd
